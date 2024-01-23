72ee1504119b2c055d19c2de3488f79ab7325299
fabiankuenzer <fabiankuenzer@gmail.com>
2024-01-23 13:09:11 +0100

    Rename response handler test

--- src/test/java/com/extension/configuration/EidSamlResponseHandlerUnitTest.java
+++ src/test/java/com/extension/configuration/EidSamlResponseHandlerUnitTest.java
@@ -37,7 +37,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class SamlResponseReceiverEndpointUnitTest {
+public class EidSamlResponseHandlerUnitTest {
 
     @Test
     void givenPersonalDataWithID_whenGetRestrictedId_thenReturnHexEncodedString() {

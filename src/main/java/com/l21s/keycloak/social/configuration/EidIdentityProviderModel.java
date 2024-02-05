c2b3ef9773aa3873e9b70570a6c8219ac8403c11
fabiankuenzer <fabiankuenzer@gmail.com>
2024-02-05 14:16:46 +0100

    Reformat EidIdentityProviderModel

--- src/main/java/com/l21s/keycloak/social/configuration/EidIdentityProviderModel.java
+++ src/main/java/com/l21s/keycloak/social/configuration/EidIdentityProviderModel.java
@@ -9,14 +9,10 @@ public class EidIdentityProviderModel extends IdentityProviderModel {
   public static final String ID_PANSTAR_SERVER_URL = "idPanstarServerUrl";
   public static final String SAML_ENTITY_BASE_URL = "samlEntityBaseUrl";
   public static final String SAML_REQUEST_SIGNATURE_PRIVATE_KEY = "samlRequestSignaturePrivateKey";
-  public static final String SAML_RESPONSE_DECRYPRION_PUBLIC_KEY =
-      "samlResponseDecryptionPublicKey";
-  public static final String SAML_RESPONSE_DECRYPRION_PRIVATE_KEY =
-      "samlResponseDecryptionPrivateKey";
-  public static final String SAML_RESPONSE_VERIFICATION_CERTIFICATE =
-      "samlResponseVerificationCertificate";
-  public static final String SAML_REQUEST_ENCRYPTION_CERTIFICATE =
-      "samlRequestEncryptionCertificate";
+  public static final String SAML_RESPONSE_DECRYPRION_PUBLIC_KEY = "samlResponseDecryptionPublicKey";
+  public static final String SAML_RESPONSE_DECRYPRION_PRIVATE_KEY = "samlResponseDecryptionPrivateKey";
+  public static final String SAML_RESPONSE_VERIFICATION_CERTIFICATE = "samlResponseVerificationCertificate";
+  public static final String SAML_REQUEST_ENCRYPTION_CERTIFICATE = "samlRequestEncryptionCertificate";
 
   private final KeycloakSession session;
 
